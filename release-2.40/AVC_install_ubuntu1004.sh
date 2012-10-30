8dd58a3f88b3317b832dee4e97932cbb7411dd37
morganbohn <morganbohn@f5520b42-8414-0410-9ae7-91ee0cd12377>
2012-10-30 13:17:06 +0000

    release 2.40
    git-svn-id: https://subversion.renater.fr/audiovideocours/documentation@1060 f5520b42-8414-0410-9ae7-91ee0cd12377

--- release-2.40/AVC_install_ubuntu1004.sh
+++ release-2.40/AVC_install_ubuntu1004.sh
@@ -50,7 +50,7 @@ echo "-------------------------------------"
 
 echo "Récupération des sources du projet"
 mkdir -p /root/src/server; cd /root/src/server
-svn co https://subversion.cru.fr/audiovideocours/tags/server/release-2.36/; mv release-2.36 univ-r_av
+svn co https://subversion.cru.fr/audiovideocours/tags/server/release-2.40/; mv release-2.40 univ-r_av
 echo "------------------------------------"
 echo "Récupération du player flash JW player"
 echo "Licence : Creative Commons : Attribution-Noncommercial-Share Alike 3.0 Unported"
